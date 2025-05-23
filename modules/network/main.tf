5bf595856712026a4065cc338b1e08188ff3c142
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2025-05-23 16:33:56 -0500

    Add subnet resources to main.tf for improved network configuration

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -4,4 +4,25 @@ resource "azurerm_virtual_network" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   tags                = var.tags
+}
+
+resource "azurerm_subnet" "first" {
+  name                 = "firstsubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.100.1.0/24"]
+}
+
+resource "azurerm_subnet" "second" {
+  name                 = "secondsubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.100.2.0/24"]
+}
+
+resource "azurerm_subnet" "third" {
+  name                 = "thirdsubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.100.3.0/24"]
 } 
