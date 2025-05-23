5382914d3100e2499da9d3fbd5df4fa641c50476
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2025-05-23 06:51:45 -0500

    Remove commented-out Azure authentication variables from main.tf for cleaner configuration

--- main.tf
+++ main.tf
@@ -12,10 +12,6 @@ terraform {
     storage_account_name = "tfstate6862"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    # client_id           = "env:ARM_CLIENT_ID"
-    # client_secret       = "env:ARM_CLIENT_SECRET"
-    # tenant_id           = "env:ARM_TENANT_ID"
-    # subscription_id     = "env:ARM_SUBSCRIPTION_ID"
   }
 }
 
