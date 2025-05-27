dcf238576479681211c204c0ff6da20978f8c6c4
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2025-05-27 08:03:19 -0500

    Add public IP resource and update outputs in compute module to include VM connection information
    - Introduced a new `azurerm_public_ip` resource for static public IP allocation.
    - Updated the network interface to associate the public IP.
    - Modified output for VM public IP to reference the new public IP resource.
    - Added a new output for VM connection information, including SSH command and sensitive details.

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -10,5 +10,15 @@ output "vm_private_ip" {
 
 output "vm_public_ip" {
   description = "The public IP address of the Virtual Machine"
-  value       = azurerm_network_interface.this.private_ip_address
+  value       = azurerm_public_ip.this.ip_address
+}
+
+output "vm_connection_info" {
+  description = "Information to connect to the VM"
+  value = {
+    username = var.vm_config.admin_username
+    host     = azurerm_public_ip.this.ip_address
+    command  = "ssh -i <private_key_file> ${var.vm_config.admin_username}@${azurerm_public_ip.this.ip_address}"
+  }
+  sensitive = true
 } 
