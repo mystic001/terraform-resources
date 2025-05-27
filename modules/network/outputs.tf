ac617fc52daca018317e3743ccecc114d53bedeb
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2025-05-27 01:59:06 -0500

    Add subnet_ids output and enhance vnet outputs in outputs.tf with descriptions for better clarity and usability

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,14 @@
+output "subnet_ids" {
+  description = "Map of subnet names to their IDs"
+  value       = { for k, v in azurerm_subnet.this : k => v.id }
+}
+
 output "vnet_id" {
-  value = azurerm_virtual_network.this.id
+  description = "The ID of the Virtual Network"
+  value       = azurerm_virtual_network.this.id
+}
+
+output "vnet_name" {
+  description = "The name of the Virtual Network"
+  value       = azurerm_virtual_network.this.name
 } 
